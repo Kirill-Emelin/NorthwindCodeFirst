9298e5dcb91a53682ca68ed0173845477e39596d
Kirill-Emelin <128322099+Kirill-Emelin@users.noreply.github.com>
2025-01-07 09:46:30 +0100

    Добавьте файлы проекта.

---
+++ NorthwindCodeFirst/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NorthwindCodeFirst
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Northwind.db");
+        }
+    }
+}
---
+++ NorthwindCodeFirst/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace NorthwindCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ApplicationContext())
+            {
+                context.Database.EnsureCreated();
+                Console.WriteLine("База данных создана, таблица Categories доступна.");
+            }
+        }
+    }
+}
